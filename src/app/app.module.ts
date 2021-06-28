bf1f9bb8f58cee52125f3ca29780b073210228de
felipe-ferraz <faff@cesar.org.br>
2021-06-28 15:51:17 -0300

    Create tooltip test project

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TooltipModule } from '@cesarbr/angular-tooltip';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    TooltipModule,
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
